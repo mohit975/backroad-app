94cada6ba32d7e454646de2dedf35a4a481a5124
Mohit Mishra <mohitmishra975@gmail.com>
2024-08-17 22:40:55 +0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <section className="hero" id="home">
       <div className="hero-banner">
-        <h1>Backroad App</h1>
+        <h1>Backroad App for booking tours </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
